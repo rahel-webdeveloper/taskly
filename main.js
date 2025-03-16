cf17371c6f3df27b3e1aa3011aa68fee1b5dfc88
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2025-03-16 08:33:04 +0430

    debug the speed insights

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import App from "./src/App";
 import Router from "./src/routes";
 
 const Main = () => {
   App();
-  SpeedInsights();
   Router.init();
 };
 
