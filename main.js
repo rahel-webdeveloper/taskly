b879b3110b90b52f4c1faa3c321d1e0bd8984b12
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2025-03-16 08:21:16 +0430

    Add speed insights

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import App from "./src/App";
 import Router from "./src/routes";
 
 const Main = () => {
   App();
+  SpeedInsights();
   Router.init();
 };
 
