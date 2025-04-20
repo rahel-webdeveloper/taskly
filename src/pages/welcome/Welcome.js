043b3227e7faa179da558d42c12ea5731be3578b
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2025-04-20 10:35:29 +0430

    Add new content and component to welcome page

--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -2,12 +2,34 @@ const Welcome = () => {
   return `
   <div class="welcome-page">
     <section class="welcome-first_section">
-      <h1>Welcome to Taskly</1h>
-      <h2><span>Track</span>, <span>Chat</span>, and <span>Conquer</span> Your Tasks!
+      <h1>Welcome to Taskly</h1>
+      <div class="first-page_subtitle"> 
+      <h2><span>Track</span>, <span>Chat</span>, and <span>Conquer</span> Your Tasks!</h2>
+
+      <p>Master your to-dos with smart tracking and instant AI advice</p>
+      </div>
+
+      <div class="welcome-btns">
+       <a href="#get-started_free">
+        <button id="get_started_btn">
+         <i class="bi bi-play"></i> Get Started Free 
+        </button>
+       </a>
+       <a href="#how-it_works">
+        <button id="how-works_btn">
+         <i class="bi bi-snow"></i> See How It Works
+        </button>
+       </a>
+      </div>
+
+      <div class="dash-img_div">
+      <img src="/public/Screenshot 2025-04-16 125640.png" class="dash-screenshot_img" />
+
+      </div>
+
     </section>
 
     <section>
-      
     </section>
 
   </div>
